29e9d355dcf9e15a63f37a832dc167767f5ee794
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-09-29 19:22:41 -0500

    Update App.js to pass CI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import { useContext } from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Auth from './components/Auth/Auth';
 import Header from './components/Header';
 import Items from './components/Items/Items';
-import { UserContext } from './context/UserProvider';
 import backgroundImage from './supermarket.jpeg';
 
 function App() {
-  const { user } = useContext(UserContext);
-
   return (
     <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <Header />
